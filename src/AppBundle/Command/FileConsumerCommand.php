da26080eaa20c23551a35067bfb5dba7f52e70d6
Franck GAMESS <hamza.almartiniki@gmail.com>
2017-08-22 15:15:13 +0200

    Update FileConsumerCommand : retrieve file content from url

--- src/AppBundle/Command/FileConsumerCommand.php
+++ src/AppBundle/Command/FileConsumerCommand.php
@@ -2,13 +2,14 @@
 
 namespace AppBundle\Command;
 
+use AppBundle\Service\FileConsumer;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class FileConsumerCommand extends Command
+class FileConsumerCommand extends ContainerAwareCommand
 {
     protected function configure()
     {
@@ -26,6 +27,19 @@ class FileConsumerCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // ...
+        $container = $this->getContainer();
+        $output->writeln(
+            [
+                'FileConsumer tool',
+                '=================',
+                '',
+            ]
+        );
+
+        $fileUrl = $input->getArgument('url');
+        $output->writeln("Trying to get the file from ".$fileUrl);
+
+        $content = $container->get(FileConsumer::class)->getFileContent($fileUrl);
+        $output->writeln('File content retrieved successfully.');
     }
 }
