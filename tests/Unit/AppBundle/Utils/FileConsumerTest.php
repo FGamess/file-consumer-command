709b855be8fd80eab8a0afe17be5777dcbe77e3a
Franck GAMESS <hamza.almartiniki@gmail.com>
2017-08-22 11:53:48 +0200

    [TESTING] add FileConsumerTest

---
+++ tests/Unit/AppBundle/Utils/FileConsumerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit\AppBundle\Service;
+
+use PHPUnit\Framework\TestCase;
+
+class FileConsumerTest extends TestCase
+{
+    /**
+     * FileConsumer
+     *
+     * @var FileConsumer
+     */
+    public $fileConsumer;
+
+    public function setUp()
+    {
+
+    }
+
+    private function initializeSUT()
+    {
+        return;
+    }
+}
