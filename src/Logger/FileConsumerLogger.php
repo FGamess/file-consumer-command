6dd8425684e56379058ac76f554ef97f7eb38433
Franck GAMESS <hamza.almartiniki@gmail.com>
2018-01-16 15:38:02 +0400

    [sf-upgrade] Upgrade structure to sf4, update docker-compose.yaml & update vendor using flex

---
+++ src/Logger/FileConsumerLogger.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Logger;
+
+use Psr\Log\LoggerInterface;
+
+class FileConsumerLogger
+{
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function outputMostFrequentWords(array $mostFrequentValues)
+    {
+        foreach ($mostFrequentValues as $word => $count) {
+            $this->logger->info($word.','.$count);
+        }
+        
+    }
+}
