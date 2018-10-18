8f42e3ea3db62ca00fd67ad6e95e9c323ca81f31
Franck-Emile Gamess <franck.gamess@gmail.com>
2018-10-18 17:15:07 +0400

    update vendors and symfony deps

--- config/bundles.php
+++ config/bundles.php
@@ -6,5 +6,4 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
